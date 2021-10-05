d46ab7d721c08bca049e3978aeab978310eb73dc
by-gramm <ready2start@naver.com>
2021-10-05 17:20:16 +0900

    Create Article model

--- diary/models.py
+++ diary/models.py
@@ -1,3 +1,10 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Article(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    diary_type = models.CharField(max_length=20)
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
