8e431a506715e95a2d3ac851ad295d13eb0e8011
by-gramm <ready2start@naver.com>
2021-10-07 00:43:11 +0900

    Add: create article_detail function in views

--- diary/urls.py
+++ diary/urls.py
@@ -6,4 +6,5 @@ url_name = 'diary'
 
 urlpatterns = [
     path('articles/', views.article_list),
+    path('articles/<int:pk>', views.article_detail),
 ]
--- diary/views.py
+++ diary/views.py
@@ -1,3 +1,5 @@
+from django.shortcuts import get_object_or_404
+from rest_framework import status
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
@@ -19,5 +21,27 @@ def article_list(request):
     else:
         serializer = ArticleSerializer(data=request.data)
         if serializer.is_valid(raise_exception=True):
-            serializer.save(user=request.user)
+            serializer.save()
         return Response(serializer.data)
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+# TODO: IsAuthorOrReadonly 데코레이터 추가
+def article_detail(request, pk):
+    article = get_object_or_404(Article, pk=pk)
+    # 게시물 상세 페이지 READ
+    if request.method == 'GET':
+        serializer = ArticleSerializer(article)
+        return Response(serializer.data)
+
+    # 게시글 UPDATE
+    elif request.method == 'PUT':
+        serializer = ArticleSerializer(article, data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+        return Response(serializer.data)
+
+    # 게시글 DELETE
+    else:
+        article.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
