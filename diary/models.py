3b0c768b22f68e68051608040ccbf9da31d078df
by-gramm <ready2start@naver.com>
2021-10-07 01:07:54 +0900

    Add: DIARY_TYPE_CHOICES

--- diary/models.py
+++ diary/models.py
@@ -3,8 +3,14 @@ from django.db import models
 
 
 class Article(models.Model):
+    DIARY_TYPE_CHOICES = (
+        (0, '메모장'),
+        (1, '원고지'),
+        (2, '어린이 일기장')
+    )
+
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    diary_type = models.CharField(max_length=20)
+    diary_type = models.SmallIntegerField(choices=DIARY_TYPE_CHOICES, default=0)
     title = models.CharField(max_length=200)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
