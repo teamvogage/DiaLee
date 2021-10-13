cb280bba554020e80bdda6864b4f981c8363f710
by-gramm <ready2start@naver.com>
2021-10-13 15:19:12 +0900

    feat: Create UserAdmin Model

--- accounts/admin.py
+++ accounts/admin.py
@@ -2,4 +2,13 @@ from django.contrib import admin
 from .models import User
 
 
-admin.site.register(User)
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = [
+        'pk',
+        'username',
+        'date_joined',
+        'email',
+        'profile_image',
+        'point',
+    ]
