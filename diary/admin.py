2653707b07ef27b400357435565d88e86681235b
by-gramm <ready2start@naver.com>
2021-10-14 21:06:52 +0900

    Add: create Comment model

--- diary/admin.py
+++ diary/admin.py
@@ -1,5 +1,24 @@
 from django.contrib import admin
-from .models import Article
+from .models import Article, Comment
 
 
-admin.site.register(Article)
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = [
+        'pk',
+        'author',
+        'diary_type',
+        'title',
+        'created_at'
+    ]
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = [
+        'pk',
+        'author',
+        'article',
+        'content',
+        'created_at'
+    ]
--- diary/models.py
+++ diary/models.py
@@ -14,3 +14,16 @@ class Article(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.content
