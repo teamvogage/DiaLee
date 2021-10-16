71c2c170952aacbdf601626e573ff6e5f4c60f22
by-gramm <ready2start@naver.com>
2021-10-17 06:26:09 +0900

    feat: Add IsAuthor custom permission class

--- diary/permissions.py
+++ diary/permissions.py
@@ -14,3 +14,14 @@ class IsAuthorOrReadonly(permissions.BasePermission):
             return True
 
         return obj.author == request.user
+
+
+class IsAuthor(permissions.BasePermission):
+    """
+    작성자에 한하여 수정(PUT) 및 삭제(DELETE)를 허용한다.
+    """
+    def has_permission(self, request, view):
+        return request.user and request.user.is_authenticated
+
+    def has_object_permission(self, request, view, obj):
+        return obj.author == request.user
