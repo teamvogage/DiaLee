00d478c54e1297f1ce0046041a1103e1c402b425
by-gramm <ready2start@naver.com>
2021-10-17 04:57:54 +0900

    feat: Set author field as read-only fields

--- diary/serializers.py
+++ diary/serializers.py
@@ -6,9 +6,11 @@ class ArticleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Article
         fields = '__all__'
+        read_only_fields = ('author',)
 
 
 class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
         fields = '__all__'
+        read_only_fields = ('author',)
