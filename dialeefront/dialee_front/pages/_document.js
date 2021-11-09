1d7b9138d0712dd5bb87c291e65aa2d53979a594
minki <uuuu66@naver.com>
2021-11-09 19:57:02 +0900

    Fix.Fix Styled-component bug

--- dialeefront/dialee_front/pages/_document.js
+++ dialeefront/dialee_front/pages/_document.js
@@ -1,19 +1,23 @@
-import Document from 'next/document'
-import { ServerStyleSheet } from 'styled-components'
+import Document from "next/document";
+import { ServerStyleSheet } from "styled-components";
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx) {
-    const sheet = new ServerStyleSheet()
-    const originalRenderPage = ctx.renderPage
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
 
     try {
+      // sheet을 사용해 정의된 모든 스타일을 수집
       ctx.renderPage = () =>
         originalRenderPage({
           enhanceApp: (App) => (props) =>
             sheet.collectStyles(<App {...props} />),
-        })
+        });
 
-      const initialProps = await Document.getInitialProps(ctx)
+      // Documents의 initial props
+      const initialProps = await Document.getInitialProps(ctx);
+
+      // props와 styles를 반환
       return {
         ...initialProps,
         styles: (
@@ -22,9 +26,9 @@ export default class MyDocument extends Document {
             {sheet.getStyleElement()}
           </>
         ),
-      }
+      };
     } finally {
-      sheet.seal()
+      sheet.seal();
     }
   }
 }
