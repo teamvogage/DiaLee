beb924d226d2ec9dd66b566cdf4c4d19eeea2366
minki <uuuu66@naver.com>
2021-11-24 21:45:49 +0900

    refactor. 로그인 시 반환하는 response interface를 수정하였음. 이제 status와 message그리고 토큰을 반환함

--- dialeefront/dialee_front/lib/axios/index.ts
+++ dialeefront/dialee_front/lib/axios/index.ts
@@ -1,4 +1,4 @@
-import axios, { AxiosError } from "axios"
+import axios, { AxiosError,AxiosResponse } from "axios"
 import {api} from "../constants"
 export interface ISendAccountData{
     [index:string]:string;
@@ -12,11 +12,14 @@ export interface ICheckData{
     "message":string,
 }
 export interface ILoginData{
+    "status":boolean,
+    "access_token":string|null,
+    "refresh_token":String|null,
     "message":string,
 }
 export const sendSignUp=async (data:ISendAccountData)=>{// 회원가입 
     try{
-        const res=await axios.post(`${api}/accounts/`,data);
+        await axios.post(`${api}/accounts/`,data);
         const goodResponse:ICheckData={
             is_valid:true,
             message:"회원가입이 완료되었습니다~! 아래 버튼을 눌러서 로그인을 해주세요.",
@@ -85,34 +88,34 @@ export const sendLogin=async(email:string,pwd:string)=>{//로그인
         password:pwd
     }
     try{
-        const res=await axios.post(`${api}/accounts/login/`,data);
-        
-        return res;
+        const res:AxiosResponse<any>=await axios.post(`${api}/accounts/login/`,data);
+        console.log(res.data);
+        const goodResponse:ILoginData={
+            status:true,
+            access_token:res.data?.access_token,
+            refresh_token:res.data?.refresh_token,
+            message:"로그인이 성공하였습니다."
+        }
+        return goodResponse;
     }catch(error){
 
-        if((error as AxiosError).response===null){
+        if(!(error as AxiosError).response){
             const NoResponse:ILoginData={
-                
+                status:false,
+                access_token:null,
+                refresh_token:null,
                 message:"인터넷 문제나 서버문제가 발생하였습니다."
             }
             return {data:NoResponse}
         }
-        if ((error as AxiosError).response?.status === 409) {
-          
-            const Conflict:ILoginData={
-                message:"중복입니다."
-            }
-            return {data:Conflict};
-        }
-        else{
             const ServerError:ILoginData={
-             
+                status:false,
+                access_token:null,
+                refresh_token:null,
                 message:"서버 오류가 발생하였습니다."
             }
             ServerError.message=`${ServerError.message}::${(error as AxiosError).response?.status}`
             return {data:ServerError};
-        }
-    
     }
 
 }
