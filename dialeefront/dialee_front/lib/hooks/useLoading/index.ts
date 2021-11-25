04359112841f63c4b7be5cf0b8ed9154bde9ba79
minki <uuuu66@naver.com>
2021-11-25 19:49:11 +0900

    test. useLoading 수정함

--- dialeefront/dialee_front/lib/hooks/useLoading/index.ts
+++ dialeefront/dialee_front/lib/hooks/useLoading/index.ts
@@ -1,10 +1,11 @@
 import { useRecoilState } from "recoil";
 import loadingState from "../../../atom/loadingState";
 const useLoading=(maxTime:number|null)=>{
-    
+
     const [isLoading,setLoading]=useRecoilState(loadingState);
     setLoading(!isLoading);
     if(maxTime)
     setTimeout(()=>setLoading(false),maxTime);
+    return isLoading
 }
 export default useLoading;
