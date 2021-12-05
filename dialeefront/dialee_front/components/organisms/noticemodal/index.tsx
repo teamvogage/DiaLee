e129031e1cab16538b802a34f50ff120002db73d
minki <uuuu66@naver.com>
2021-12-05 19:32:26 +0900

    style.notice modal 로딩 추가

--- dialeefront/dialee_front/components/organisms/noticemodal/index.tsx
+++ dialeefront/dialee_front/components/organisms/noticemodal/index.tsx
@@ -5,11 +5,15 @@ import Button from "../../atoms/button"
 import Span from "../../atoms/span"
 import Image from "next/dist/client/image"
 import { ComponentProps } from "react"
+import useLoading from "../../../lib/hooks/useLoading"
 const NoticeModal=({setNotice,message}:ComponentProps<any>)=>{
+    const {loadingFake }= useLoading();
     const onClick=()=>{
         setNotice(false);
+        loadingFake(4000);
         Router.push('/');
     }
+
     return(
     <Modal  animated={false}  title="로그인 해야됨." confirmBtn={<Button btn_type="cancle" onClick={onClick} >오케이 </Button>}  zIndex={8000} isCancle="no">
         
